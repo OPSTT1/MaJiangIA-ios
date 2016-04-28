c3f48e84b4300cb277c243644221814f266c3407
lihaomiao <lihaomiao@papayamobile.cn>
2016-04-28 13:40:35 +0800

    Initial Commit

---
+++ MajiangAI/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  MajiangAI
+//
+
+//  Copyright (c) 2016年 Li Haomiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
